5358f94a08491c4c82a97c87c70557e7a4e6f602
Demian <53890358+demian35@users.noreply.github.com>
2023-03-05 01:08:39 -0600

    intalando dependencia de nodemon

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Telegraf=require('telegraf')
 
-const bot=new Telegraf('')
+const bot=new Telegraf('6096245384:AAF_YWgdjjH2cAkhO47xiQDdHikAYJZLu4Y')
+
+bot.start((ctx)=>{
+    ctx.reply("")
+})
